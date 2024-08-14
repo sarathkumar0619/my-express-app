1c70c96d25f51033a2e28b73d9363b825f85a87d
sarathkumar0619 <130230309+sarathkumar0619@users.noreply.github.com>
2024-08-14 17:03:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import userRoutes from './routes/users.js';
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+app.use('/users', userRoutes);
+
+app.get('/', (req, res) => res.send('HELLO FROM HOMEPAGE'));
+
+app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
